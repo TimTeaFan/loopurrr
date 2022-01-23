ce04016ec06b75c08e734deaf264e7cbc0cd2364
TimTeaFan <github@e-niversity.com>
2022-01-23 22:58:26 +0100

    clean: rearranged order of functions

--- R/output_options.R
+++ R/output_options.R
@@ -1,4 +1,39 @@
 
+get_output_opt <- function(default = NULL) {
+  getOption("loopurrr.output", default = default)
+}
+
+set_output_opt <- function(x = list("rstudio", "clipboard", "console", NULL)) {
+  match.arg(x, several.ok = TRUE)
+  if (any(purrr::map_lgl(x, is.null)) && length(x) > 1) {
+    rlang::abort(
+      c("Problem with `set_output_opt()` input `x`.",
+        i = "You can either unset {loopurrr}'s output option by specifying `x = NULL` or you can set it to one or several output options.",
+        x = "`x` contains `NULL` as well as other output options.",
+        i = "`x = NULL` must not be used together with other output options.")
+    )
+  }
+  quoted_option <- bquote(options("loopurrr.output" = .(x)))
+  option_chr <- gsub('"', "'", deparse(quoted_option))
+  x_chr <- gsub('"', "'", deparse(x))
+  eval(quoted_option)
+  rlang::inform(paste0('{loopurrr}s output option has been temporarily set to: ', x_chr, '.',
+                       if(all(!is.null(x))) {
+                         paste0('\nTo set this option permanently add `', option_chr, '` to your .Rprofile.')
+                       })
+  )
+}
+
+default_context <- function() {
+  out_opt <- get_output_opt(default = c("rstudio", "clipboard"))
+  if(!"console" %in% out_opt) {
+    out_opt <- c(out_opt, "console")
+  }
+  out_opt
+}
+
+
+
 create_output_fn <- function(output) {
 
   has_rstudioapi <- requireNamespace("rstudioapi", quietly = TRUE)
@@ -73,35 +108,4 @@ create_output_fn <- function(output) {
   out_fn
 }
 
-get_output_opt <- function(default = NULL) {
-  getOption("loopurrr.output", default = default)
-}
 
-set_output_opt <- function(x = list("rstudio", "clipboard", "console", NULL)) {
-  match.arg(x, several.ok = TRUE)
-  if (any(purrr::map_lgl(x, is.null)) && length(x) > 1) {
-    rlang::abort(
-      c("Problem with `set_output_opt()` input `x`.",
-        i = "You can either unset {loopurrr}'s output option by specifying `x = NULL` or you can set it to one or several output options.",
-        x = "`x` contains `NULL` as well as other output options.",
-        i = "`x = NULL` must not be used together with other output options.")
-    )
-  }
-  quoted_option <- bquote(options("loopurrr.output" = .(x)))
-  option_chr <- gsub('"', "'", deparse(quoted_option))
-  x_chr <- gsub('"', "'", deparse(x))
-  eval(quoted_option)
-  rlang::inform(paste0('{loopurrr}s output option has been temporarily set to: ', x_chr, '.',
-                       if(all(!is.null(x))) {
-                         paste0('\nTo set this option permanently add `', option_chr, '` to your .Rprofile.')
-                       })
-  )
-}
-
-default_output <- function() {
-  out_opt <- get_output_opt(default = c("rstudio", "clipboard"))
-  if(!"console" %in% out_opt) {
-    out_opt <- c(out_opt, "console")
-  }
-  out_opt
-}
