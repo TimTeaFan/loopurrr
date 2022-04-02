07561c0f7ebf34a1d63f7f3b61103fb204a916bc
TimTeaFan <github@e-niversity.com>
2022-04-02 22:10:06 +0200

    dependencies: importFrom("methods", "findFunction")

--- R/loopurrr-package.R
+++ R/loopurrr-package.R
@@ -7,5 +7,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom("methods", "findFunction")
 ## usethis namespace: end
 NULL
