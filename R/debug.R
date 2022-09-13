2cde04e1a64a8f862e143a136b746b2dc46e0d74
TimTeaFan <github@e-niversity.com>
2022-09-13 22:04:12 +0200

    feature: add functions `ping()` and `wrap()` to manipulate purrr calls

--- R/debug.R
+++ R/debug.R
@@ -1,49 +1,42 @@
-ping <- function(.expr, i = 1L) {
+# TODO: document and export this function
+# TODO: add tests
+# Tests:
+# a <- list(1, 1:2, 1:3)
+# map(a, sum) %>% ping(1)
+# map(a, sum) %>% ping(c(1,3))
+# map(a, sum) %>% ping(simplify = FALSE)
+# ping rewritten based on wrap
+ping <- function(.expr, i = 1L, simplify = TRUE) {
+
+  check_names <- check_i(i)
 
   q <- rlang::enquo(.expr)
 
   # check and unpipe
-  new_expr <- check_and_unpipe(sys.calls(),
-                               is_dot = match.call()$`.expr` == ".",
-                               calling_fn = "ping")
+  q <- unpipe_expr(q,
+                   sc = sys.calls(),
+                   is_dot = match.call()$`.expr` == ".",
+                   calling_fn = "ping2")
 
-  if (!is.null(new_expr)) {
-    q <- rlang::quo_set_expr(q, new_expr)
+  out <- wrap(.expr = q,
+              .x = index_fn(i),
+              .y = index_fn(i),
+              .l = index_each_fn(i),
+              silent = TRUE,
+              `.__impl__.` = TRUE,
+              `.__check__.`= check_names
+  )
+
+  if (simplify && is.list(out) && length(out) == 1L) {
+    out <- unlist(out, recursive = FALSE)
   }
 
-  q_expr <- rlang::quo_get_expr(q)
-  q_env <- rlang::quo_get_env(q)
-
-  map_fn_chr <- deparse(q_expr[[1]])
-
-  if (grepl("^\\w+::", map_fn_chr, perl = TRUE)) {
-    map_fn_chr <- gsub("^\\w+::", "", map_fn_chr)
-  }
-
-  map_fn <- get(map_fn_chr, envir = rlang::as_environment("purrr"))
-  q_ex_std <- match.call(definition = map_fn, call = q_expr)
-
-  expr_ls <- as.list(q_ex_std)
-
-  if (!is.null(expr_ls[[".x"]])) {
-    expr_ls[[".x"]] <- rlang::call2(`[`, expr_ls[[".x"]], i)
-  }
-
-  if (!is.null(expr_ls[[".y"]])) {
-    expr_ls[[".y"]] <- rlang::call2(`[`, expr_ls[[".y"]], i)
-  }
-
-  if (!is.null(expr_ls[[".l"]])) {
-
-    index_each_el <- rlang::as_function(~ purrr::map(.x, function(x) `[`(x, i)))
-
-    expr_ls[[".l"]] <- rlang::call2(index_each_el, expr_ls[[".l"]])
-  }
-
- eval(as.call(expr_ls))
+  out
 
 }
 
+
+# TODO: Make function work with the first and last element `.f` (and or `.else`) was applied to:
 first_and_last <- function(x) {
   ln_x <- length(x)
   if (ln_x > 1) {
@@ -51,20 +44,45 @@ first_and_last <- function(x) {
   } else if (ln_x == 1L) {
     1L
   } else {
-    stop("Object must of 1 or more elements.")
+    stop("Object must have 1 or more elements.")
   }
 }
 
-wrap_.f <- function(.expr, fn) {
-  q <- rlang::enquo(.expr)
+index_fn <- function(j) {
+  function(x, i = j) `[`(x, i)
+}
 
-  # check and unpipe
-  new_expr <- check_and_unpipe(sys.calls(),
-                               is_dot = match.call()$`.expr` == ".",
-                               calling_fn = "wrap_.f")
+index_each_fn <- function(j) {
+  function(x, i = j) {
+    purrr::map(x, function(y) `[`(y, i))
+  }
+}
 
-  if (!is.null(new_expr)) {
-    q <- rlang::quo_set_expr(q, new_expr)
+wrap <- function(.expr, ..., silent = FALSE) {
+
+  dots <- rlang::list2(...)
+  implement <- !is.null(dots[[".__impl__."]])
+  i_names <- dots[[".__check__."]]
+  check_names <- !is.null(i_names)
+  dots <- dots[-which(names(dots) %in% c(".__impl__.", ".__check__."))]
+
+  if (!rlang::is_named(dots)) {
+    stop("All arguments in the ellipsis `...` must be named.")
+  }
+
+  # `.__impl__.` is a hidden arg used when other functions call `wrap`
+  # in this case .expr is already a preprocessed quosure
+  # Tje arg it isn't needed when the user calls `wrap()` directly
+  if (implement) {
+    q <- .expr
+  } else {
+    q <- rlang::enquo(.expr)
+
+    # check and unpipe
+    q <- unpipe_expr(q,
+                     sc = sys.calls(),
+                     is_dot = match.call()$`.expr` == ".",
+                     calling_fn = "wrap")
   }
 
   q_expr <- rlang::quo_get_expr(q)
@@ -81,7 +99,103 @@ wrap_.f <- function(.expr, fn) {
 
   expr_ls <- as.list(q_ex_std)
 
-  expr_ls[[".f"]] <- rlang::call2(fn, expr_ls[[".f"]])
+  if (check_names && !all(i_names %in% names(eval(expr_ls[[".x"]], envir = q_env)))) {
+    stop("Problem with ping(). Not all names in object `.x`")
+  }
+
+  for (arg in names(dots)) {
+    if (!arg %in% names(expr_ls)) {
+     if(silent) next else stop(paste0("Argument `", arg, "` doesn't exist in `.expr`."))
+    }
+    expr_ls[[arg]] <- rlang::call2(dots[[arg]], expr_ls[[arg]])
+  }
 
   eval(as.call(expr_ls))
 }
+
+
+# implement a function like this
+# but including input class and output class
+# https://github.com/tidyverse/purrr/issues/843
+# screen
+
+# wrap_.f <- function(.expr, fn) {
+#
+#   q <- rlang::enquo(.expr)
+#
+#   # check and unpipe
+#   new_expr <- check_and_unpipe(sys.calls(),
+#                                is_dot = match.call()$`.expr` == ".",
+#                                calling_fn = "wrap_.f")
+#
+#   if (!is.null(new_expr)) {
+#     q <- rlang::quo_set_expr(q, new_expr)
+#   }
+#
+#   q_expr <- rlang::quo_get_expr(q)
+#   q_env <- rlang::quo_get_env(q)
+#
+#   map_fn_chr <- deparse(q_expr[[1]])
+#
+#   if (grepl("^\\w+::", map_fn_chr, perl = TRUE)) {
+#     map_fn_chr <- gsub("^\\w+::", "", map_fn_chr)
+#   }
+#
+#   map_fn <- get(map_fn_chr, envir = rlang::as_environment("purrr"))
+#   q_ex_std <- match.call(definition = map_fn, call = q_expr)
+#
+#   expr_ls <- as.list(q_ex_std)
+#
+#   expr_ls[[".f"]] <- rlang::call2(fn, expr_ls[[".f"]])
+#
+#   eval(as.call(expr_ls))
+# }
+#
+# old_ping <- function(.expr, i = 1L, simplify = TRUE) {
+#
+#   q <- rlang::enquo(.expr)
+#
+#   # check and unpipe
+#   q <- unpipe_expr(q,
+#                    sc = sys.calls(),
+#                    is_dot = match.call()$`.expr` == ".",
+#                    calling_fn = "ping")
+#
+#   q_expr <- rlang::quo_get_expr(q)
+#   q_env <- rlang::quo_get_env(q)
+#
+#   map_fn_chr <- deparse(q_expr[[1]])
+#
+#   if (grepl("^\\w+::", map_fn_chr, perl = TRUE)) {
+#     map_fn_chr <- gsub("^\\w+::", "", map_fn_chr)
+#   }
+#
+#   map_fn <- get(map_fn_chr, envir = rlang::as_environment("purrr"))
+#   q_ex_std <- match.call(definition = map_fn, call = q_expr)
+#
+#   expr_ls <- as.list(q_ex_std)
+#
+#   if (!is.null(expr_ls[[".x"]])) {
+#     expr_ls[[".x"]] <- rlang::call2(`[`, expr_ls[[".x"]], i)
+#   }
+#
+#   if (!is.null(expr_ls[[".y"]])) {
+#     expr_ls[[".y"]] <- rlang::call2(`[`, expr_ls[[".y"]], i)
+#   }
+#
+#   if (!is.null(expr_ls[[".l"]])) {
+#
+#     index_each_el <- rlang::as_function(~ purrr::map(.x, function(x) `[`(x, i)))
+#
+#     expr_ls[[".l"]] <- rlang::call2(index_each_el, expr_ls[[".l"]])
+#   }
+#
+#   out <- eval(as.call(expr_ls))
+#
+#   if (simplify && is.list(out) && length(out) == 1L) {
+#     out <- unlist(out, recursive = FALSE)
+#   }
+#
+#   out
+#
+# }
