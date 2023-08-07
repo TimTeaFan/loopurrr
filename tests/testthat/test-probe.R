1623fac6a8479305fe39b744bb373f10179130b5
TimTeaFan <github@e-niversity.com>
2023-08-07 22:44:18 +0200

    test: add test for probe's stop_at argument

--- tests/testthat/test-probe.R
+++ tests/testthat/test-probe.R
@@ -42,6 +42,12 @@ test_that("with error: works with map", {
   )
 
   expect_equal(
+    list(.x = 15,
+         .y = "a"),
+    reduce(b, sum) %>% probe()
+  )
+
+  expect_equal(
     list(.x = 115,
          .y = "a"),
     accumulate(b, sum, .init = 100) %>% probe()
@@ -68,6 +74,24 @@ test_that("without error: works with map", {
   expect_equal(
     3,
     reduce(1:10, sum) %>% probe()
+  )
+
+  expect_equal(
+    101,
+    reduce(1:10, sum, .init = 100) %>% probe()
+  )
+})
+
+test_that("without error and stop_at: works with map", {
+
+  expect_equal(
+    7:9,
+    map(list(1:3, 4:6, 7:9), sum) %>% probe(stop_at = \(x) x > 20)
+  )
+
+  expect_equal(
+    3,
+    reduce(1:10, sum) %>% probe(stop_at = \(x) x > 20)
   )
 
   expect_equal(
