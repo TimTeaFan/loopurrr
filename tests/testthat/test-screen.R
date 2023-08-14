fa53d4166b8b7a1f546e9bf4d37da3af8997b111
TimTeaFan <github@e-niversity.com>
2023-08-14 13:02:33 +0200

    test: accumulate

--- tests/testthat/test-screen.R
+++ tests/testthat/test-screen.R
@@ -6,7 +6,7 @@ test_that("screen works with map", {
     } else if (a == 2){
       warning("a warning")
       return(a)
-    } else if (a == 3){
+    } else if (a == "a"){
       print("I'm here")
       message("This is a message")
       warning("A warning followed by ...")
@@ -21,30 +21,74 @@ test_that("screen works with map", {
     }
   }
 
-  a = 1:6
+  a = flatten(list(1:2, "a", 4:6))
 
-  output_tbl <- structure(
-    list(input = 1:5,
-         inp_class = c("integer", "integer", "integer", "integer", "integer"),
-         result = c("1", NA, NA, "5", "5"),
-         res_class = c("integer", "logical", "logical", "character", "integer"),
-         error = c(NA, "an error", NA, NA, NA),
-         output = c("", "", "", "[1] \"5\"", ""),
-         warning = c(NA, NA, "this is a message", NA, NA),
-         message = c("a warning", NA, NA, NA, NA)),
-    row.names = c(NA, -5L),
-    class = c("screen_tbl", "tbl_df", "tbl", "data.frame"),
-    call = "`map(a, myf)`",
-    no_err = 1L,
-    perc_err = 20,
-    no_warn = 1L,
-    perc_warn = 20,
-    no_of_classes = 2L,
-    class_output = "[1] integer, [2] character")
-
-  map(a, myf) %>% screen()
+  output_tbl <- structure(list(
+    input_x = c("1", "2", "a", "4", "5", "6"),
+    input_x_class = c("integer","integer", "character", "integer", "integer", "integer"),
+    result = list(1L, 2L, NULL, NULL, tibble::tibble(id = 1:5, letter = c("a","b", "c", "d", "e")), "6"),
+    result_class = list("integer", "integer", "NULL", "NULL", c("tbl_df", "tbl", "data.frame"), "character"),
+    error = c(NA, NA, "an error", NA, NA, NA),
+    output = c("", "", "[1] \"I'm here\"", "", "", "[1] \"6\""),
+    warning = c(NA, "a warning", "A warning followed by ...", NA, NA, NA),
+    message = c(NA, NA, "This is a message\n", "this is a message", NA, NA)
+  ),
+  row.names = c(NA, -6L),
+  class = c("screen_tbl", "tbl_df", "tbl", "data.frame"),
+  call = "`map(a, myf)`",
+  has_fn = TRUE,
+  no_err = 1L,
+  perc_err = 17,
+  no_warn = 2L,
+  perc_warn = 33,
+  no_of_classes = 4L,
+  class_output = "[1] integer, [2] NULL, [3] tbl_df, tbl, data.frame, [4] character"
+  )
 
   expect_equal(output_tbl,
                screen(map(a, myf))
-              )
+               )
+})
+
+test_that("screen works with accumulate", {
+
+  mysum <- function(x, y ){
+
+    res <- x + y
+
+    if (res %% 2) warning("This is a warning")
+
+
+    if (res %/% 4) print("Result is getting big")
+
+    res
+  }
+
+  a = flatten(list(1:2, "a", 4:6))
+
+  output_tbl <- structure(list(
+    input_x = c("1", "2", "a", "4", "5", "6"),
+    input_x_class = c("integer","integer", "character", "integer", "integer", "integer"),
+    result = list(1L, 2L, NULL, NULL, tibble::tibble(id = 1:5, letter = c("a","b", "c", "d", "e")), "6"),
+    result_class = list("integer", "integer", "NULL", "NULL", c("tbl_df", "tbl", "data.frame"), "character"),
+    error = c(NA, NA, "an error", NA, NA, NA),
+    output = c("", "", "[1] \"I'm here\"", "", "", "[1] \"6\""),
+    warning = c(NA, "a warning", "A warning followed by ...", NA, NA, NA),
+    message = c(NA, NA, "This is a message\n", "this is a message", NA, NA)
+  ),
+  row.names = c(NA, -6L),
+  class = c("screen_tbl", "tbl_df", "tbl", "data.frame"),
+  call = "`map(a, myf)`",
+  has_fn = TRUE,
+  no_err = 1L,
+  perc_err = 17,
+  no_warn = 2L,
+  perc_warn = 33,
+  no_of_classes = 4L,
+  class_output = "[1] integer, [2] NULL, [3] tbl_df, tbl, data.frame, [4] character"
+  )
+
+  expect_equal(output_tbl,
+               screen(map(a, myf))
+  )
 })
