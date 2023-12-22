1e8933f4e95e3d33a2bee937ca5877b24f5879e6
TimTeaFan <github@e-niversity.com>
2023-12-22 14:19:32 +0100

    fix: screen and make it work with reduce

--- R/screen.R
+++ R/screen.R
@@ -1,35 +1,27 @@
-# TODO: make screen and capture_env work with map_if and map_at
-# 1. get .at, .p and .else
-# 2. get index => with purrr::as_mapper
-
-# TODO: make screen and capture_env work with accu and redu
-# TODO: map_dbl throws error despite `screen`
+# TODO:
+# add `f` column to result data.frame to show where f or else was exectuded
 
 #' @export
-screen <- function(expr, res_fns = list(class), inp_fns = list(class), max_inp_no = 2) {
+screen <- function(expr,
+                   res_fns = list(class),
+                   inp_fns = list(class),
+                   max_inp_no = 2) {
 
+  # checks and setup
   check_pos_integer(max_inp_no, "screen", "max_inp_no")
 
-  q         <- rlang::enquo(expr)
-  q_res_fns <- rlang::enquo(res_fns)
-  q_inp_fns <- rlang::enquo(inp_fns)
+  q              <- rlang::enquo(expr)
+  q_res_fns      <- rlang::enquo(res_fns)
+  q_inp_fns      <- rlang::enquo(inp_fns)
 
-  if (!is.null(res_fns)) {
-    check_list(res_fns, "screen", "res_fns")
-    map(res_fns, ~ check_functionable(.x, "screen", "res_fns"))
-    res_fns <- transform_fn(res_fns, q_res_fns, "res_fns")
-  }
+  res_fns        <- check_and_maybe_transform(res_fns, q_res_fns, "res_fns")
 
-  if (!is.null(inp_fns)) {
-    check_list(inp_fns, "screen", "inp_fns")
-    map(inp_fns, ~ check_functionable(.x, "screen", "inp_fns"))
-    inp_fns <- transform_fn(inp_fns, q_inp_fns, "inp_fns")
-  }
+  inp_fns        <- check_and_maybe_transform(inp_fns, q_inp_fns, "inp_fns")
 
-  q <- unpipe_expr(q,
-                   sc = sys.calls(),
-                   is_dot = match.call()$`expr` == ".",
-                   calling_fn = "screen")
+  q              <- unpipe_expr(q,
+                                sc = sys.calls(),
+                                is_dot = match.call()$`expr` == ".",
+                                calling_fn = "screen")
 
   q_expr         <- rlang::quo_get_expr(q)
   q_env          <- rlang::quo_get_env(q)
@@ -46,93 +38,47 @@ screen <- function(expr, res_fns = list(class), inp_fns = list(class), max_inp_n
   at_idx         <- eval(expr_ls[[".at"]], envir = q_env)
   p_fn           <- eval_as_fn(expr_ls[[".p"]], env = q_env)
   else_fn        <- eval_as_fn(expr_ls[[".else"]], env = q_env)
+  init           <- eval(expr_ls[[".init"]], envir = q_env)
 
   is_accu_redu   <- grepl("^(accumulate|reduce)", map_fn_chr, perl = TRUE)
   is_accu_redu2  <- grepl("^(accumulate|reduce)2$", map_fn_chr, perl = TRUE)
   is_back        <- !is.null(expr_ls[[".dir"]]) && expr_ls[[".dir"]] == "backward"
   is_redu        <- grepl("^reduce", map_fn_chr, perl = TRUE)
   has_init       <- !is.null(expr_ls[[".init"]])
+  is_typed       <- check_if_typed(map_fn_chr)
 
   inp_objs       <- get_inp_objs(expr_ls, q_env, max_inp_no, is_accu_redu2, has_init)
   has_fn         <- !is.null(expr_ls[[".f"]])
+  has_at_or_p    <- !is.null(expr_ls[[".at"]]) || !is.null(expr_ls[[".p"]])
 
-  # change to accumulate if reduce
-  if (is_redu) {
-    expr_ls      <- reduce2accumulate(expr_ls)
-    q            <- rlang::quo_set_expr(q, as.call(expr_ls))
-  }
+  q              <- adjust_quo_if_reduce(q, expr_ls, is_redu)
 
+  use_env        <- is_accu_redu | is_typed
+
+  # actual function:
+  # if map call has a function
   if (has_fn) {
 
-    no_i         <- infer_possible_i(inp_objs[[1]], is_accu_redu, has_init)
-    prepare_capture_env(no_i)
+    maybe_prepare_capture_env(inp_objs[[1]], is_accu_redu, has_init, use_env)
 
-    res <- wrap(..expr    = !! q,
-                .f        = function(x) capture(x, env = is_accu_redu),
-                ..silent  = TRUE)
+    res          <- wrap_and_try(q, use_env, is_typed, map_fn_chr)
 
-    res <- restore(res, has_init, is_back, env = is_accu_redu)
+    res          <- restore(res, has_init, is_back, env = is_accu_redu)
 
-    inp_objs    <- finalize_inp_obj(inp_objs, res, is_accu_redu, has_init, is_back)
-
-    init_res    <- tibble::tibble(inp_objs,
-                                  res)
-
+    # if functionless map call
   } else {
 
-    init_res <- tibble::new_tibble(inp_objs, nrow = length(inp_objs[[1]]))
-    # <<<<< put this part in helper function
-    if (max_inp_no == 0) {
-      rlang::warn(c(i = paste0("`screen()` returning `NULL`, because:"),
-                    paste0("\033[32m", "\u23F5", "\033[39m", " `max_inp_no` is set to `0` AND"),
-                    paste0("\033[32m", "\u23F5", "\033[39m", " `expr` doesn't contain a function in `.f`.")
-                    )
-      )
-    } else {
-
-      if (is_accu_redu || is_accu_redu2) {
-        msg <- "Returning `.x` input only:"
-      } else {
-        msg <- "Returning only inputs:"
-      }
-
-      rlang::inform(c(paste0("Screening call: ", cl_chr),
-                      i = "`.f` argument not supplied.",
-                      i = msg))
-    }
-    # >>>>>>
-  }
-
-  # <<<<< put this part in helper function
-  out <- dplyr::rowwise(init_res)
-
-  if (!is.null(inp_fns)) {
-    out <- dplyr::mutate(out,
-                         dplyr::across(tidyselect::starts_with("input"), inp_fns))
-
-    if (has_fn) {
-      out <- dplyr::relocate(out, starts_with("input"), .before = result)
-    }
+    warn_inform_return(max_inp_no, is_accu_redu, cl_chr)
+    res <- NULL
 
   }
 
-  if (has_fn && !is.null(res_fns)) {
-    out <- dplyr::mutate(out,
-                         dplyr::across(tidyselect::starts_with("result"), res_fns),
-                         .before = error)
-  }
+  init_res       <- finalize_inp_obj(inp_objs, res, is_accu_redu, has_init, is_back, init, q_env)
 
-  out <- dplyr::ungroup(out)
+  out            <- finalize_output(init_res, res, inp_fns, res_fns, has_fn, cl_chr)
 
-  out2 <- dplyr::mutate(out,
-                        dplyr::across(! matches("^input_[x,y]$") &
-                                      ! matches("^input_l\\d+$") &
-                                      ! matches("^result$"),
-                                      ~ unlist_if_all_length_one(.x, dplyr::cur_column())
-                        )
-  )
-  attach_screen_attr(out2, cl_chr, has_fn)
-  # >>>>>>
+  out
+
 }
 
 attach_screen_attr <- function(x, cl_chr, has_fn) {
@@ -308,7 +254,6 @@ get_inp_objs <- function(expr_ls, q_env, max_inp_no, is_accu_redu2, has_init) {
 
   if (!is.null(expr_ls[[".x"]]) && max_inp_no == 1) {
 
-    # out <- tibble::new_tibble(list(input_x = out), nrow = length(out))
     out <- list(input_x = out)
     return(out)
   }
@@ -316,9 +261,9 @@ get_inp_objs <- function(expr_ls, q_env, max_inp_no, is_accu_redu2, has_init) {
   if (!is.null(expr_ls[[".y"]]) && max_inp_no >= 2) {
 
     inp_y    <- eval(expr_ls[[".y"]], envir = q_env)
-    tmp      <- list(inp_x, inp_y)
+    tmp      <- list(out, inp_y)
     named_xy <- set_names(tmp, paste0("input_", c("x", "y")))
-    # out      <- tibble::new_tibble(named_xy, nrow = length(inp_x))
+
     return(named_xy)
   }
 
@@ -327,29 +272,178 @@ get_inp_objs <- function(expr_ls, q_env, max_inp_no, is_accu_redu2, has_init) {
     tmp      <- eval(expr_ls[[".l"]], envir = q_env)
     inp_l    <- tmp[seq_len(max_inp_no)]
     named_l  <- set_names(inp_l, paste0("input_l", seq_len(max_inp_no)))
-    # out      <- tibble::new_tibble(named_l, nrow = length(inp_l[[1]]))
+
     return(named_l)
   }
 
-  # out <- tibble::new_tibble(list(input_x = out), nrow = length(out))
   out <- list(input_x = out)
 
 }
 
 # accounts for accumulate and reduce and turns input list into tibble
-finalize_inp_obj <- function(inp_objs, res, is_accu_redu, has_init, is_back) {
+finalize_inp_obj <- function(inp_objs, res, is_accu_redu, has_init, is_back, init, q_env) {
 
-  if (is_accu_redu) {
+  if (is_accu_redu && !is.null(res)) {
+    inp_objs <- as.list(inp_objs)
+
     if (has_init) {
-      inp_objs <- as.list(inp_objs)
-      inp_objs[["input_y"]] <- append(list(NULL), inp_objs[["input_x"]])
+
+      if (is_back) {
+        inp_objs[["input_y"]] <- append(inp_objs[["input_x"]], init)
+      } else {
+        inp_objs[["input_y"]] <- append(init, inp_objs[["input_x"]])
+      }
+
+      } else {
+        inp_objs[["input_y"]] <- inp_objs[["input_x"]]
+      }
+
+    if (is_back) {
+      inp_objs[["input_x"]] <- append(res$result[-1], list(NULL))
     } else {
-      inp_objs[["input_y"]] <- inp_objs[["input_x"]] # append(list(NULL), inp_objs[["input_x"]][-1L])
+      inp_objs[["input_x"]] <- append(list(NULL), res$result[-nrow(res)])
     }
 
-    inp_objs[["input_x"]] <- append(list(NULL), res$result[-nrow(res)]) # res$result[[1]],
-    # if(!tibble::is_tibble(inp_objs)) inp_objs <- tibble::as_tibble(inp_objs)
   }
 
-  tibble::new_tibble(inp_objs, nrow = length(inp_objs[[1]]))
+  out <- tibble::new_tibble(inp_objs,
+                            nrow = length(inp_objs[[1]]))
+
+  dplyr::mutate(out, i = add_i(is_back, is_accu_redu), .before = 1L)
+}
+
+adjust_quo_if_reduce <- function(q, expr_ls, is_redu) {
+
+  if (is_redu) {
+    expr_ls      <- reduce2accumulate(expr_ls)
+    q            <- rlang::quo_set_expr(q, as.call(expr_ls))
+  }
+
+  q
+}
+
+warn_inform_return <- function(max_inp_no, is_accu_redu, cl_chr) {
+
+  if (max_inp_no == 0) {
+
+    rlang::warn(c(i = paste0("`screen()` returning `NULL`, because:"),
+                  paste0("\033[32m", "\u23F5", "\033[39m", " `max_inp_no` is set to `0` AND"),
+                  paste0("\033[32m", "\u23F5", "\033[39m", " `expr` doesn't contain a function in `.f`.")
+                  )
+                )
+
+  } else {
+
+    if (is_accu_redu) {
+      msg <- "Returning `.x` input only:"
+    } else {
+      msg <- "Returning only inputs:"
+    }
+
+    rlang::inform(c(paste0("Screening call: ", cl_chr),
+                    i = "`.f` argument not supplied.",
+                    i = msg))
+  }
+
+}
+
+finalize_output <- function(init_res, res, inp_fns, res_fns, has_fn, cl_chr) {
+
+  init_res <- tibble::tibble(init_res, res)
+
+  out <- dplyr::rowwise(init_res)
+
+  if (!is.null(inp_fns)) {
+    out <- dplyr::mutate(out,
+                         dplyr::across(tidyselect::starts_with("input"), inp_fns))
+
+    if (has_fn) {
+      out <- dplyr::relocate(out, starts_with("input"), .before = result)
+    }
+
+  }
+
+  if (has_fn && !is.null(res_fns)) {
+    out <- dplyr::mutate(out,
+                         dplyr::across(tidyselect::starts_with("result"), res_fns),
+                         .before = error)
+  }
+
+  out <- dplyr::ungroup(out)
+
+  out <- dplyr::mutate(out,
+                       dplyr::across(! matches("^input_[x,y]$") &
+                                     ! matches("^input_l\\d+$") &
+                                     ! matches("^result$"),
+                                     ~ unlist_if_all_length_one(.x, dplyr::cur_column())
+                        )
+  )
+  attach_screen_attr(out, cl_chr, has_fn)
+}
+
+check_and_maybe_transform <- function(fns, fns_quo, arg_nm) {
+
+  if (!is.null(fns)) {
+    check_list(fns, "screen", arg_nm)
+    map(fns, ~ check_functionable(.x, "screen", arg_nm))
+    fns <- transform_fn(fns, fns_quo, arg_nm)
+  }
+
+  fns
+}
+
+check_if_typed <- function(map_fn_chr){
+
+  get_output_type(map_fn_chr) %in% c("dbl", "int", "lgl", "raw", "chr")
+
+}
+
+wrap_and_try <- function(q, use_env, is_typed, map_fn_chr) {
+
+  res <- tryCatch({
+    wrap(..expr    = !! q,
+       .f        = function(x) capture(x, env = use_env),
+       .else     = function(x) quietly(safely(x)),
+       ..silent  = TRUE)
+    }, error = function(e) {
+      e
+    })
+
+  if (rlang::is_error(res)) {
+
+    init_msg <- c(
+      "Problem with `screen()` input `expr`.",
+      i = "`screen()` only captures errors thrown by the functions supplied to `.f`.",
+      x = paste0("An error in the setup of the overarching call to `", map_fn_chr, "` has been detected.")
+      )
+
+    if (is_typed) {
+      unytped_map_fn <- gsub("_.*$", "", map_fn_chr)
+
+      add_msg <- c(i = paste0("Try using the untyped version of your call: `",
+                              unytped_map_fn , "`."))
+
+    } else {
+      add_msg <- c(i = paste0(
+        "Please check if you correctly supplied the following arguments to `", map_fn_chr, "`:"),
+        paste0("\033[32m", "\u23F5", "\033[39m", " a vector object to `.x`"),
+        paste0("\033[32m", "\u23F5", "\033[39m", " a function (or string, integer or list) to `.f`")
+        )
+    }
+    final_msg <- append(init_msg, add_msg)
+
+    rlang::abort(final_msg)
+  }
+
+  res
+
+}
+
+maybe_prepare_capture_env <- function(inp_objs, is_accu_redu, has_init, use_env) {
+
+  if (use_env) {
+    no_i       <- infer_possible_i(inp_objs, is_accu_redu, has_init)
+    prepare_capture_env(no_i)
+  }
+
 }
